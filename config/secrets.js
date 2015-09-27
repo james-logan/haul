7339ff0dc0d6441c4a1a625e7dcfa134fa113522
James Logan <jamesblogan20@gmail.com>
2015-09-26 20:11:54 -0500

    removed secrets file, added CNAME. Site now hosted

--- config/secrets.js
+++
@@ -1,8 +0,0 @@
-
-
-var databaseName = process.env.NODE_ENV === 'test' ? 'hauldb-test' : 'haul';
-
-module.exports = {
-  db: process.env.MONGODB_URL || 'mongodb://localhost:27017/' + databaseName,
-  session: process.env.SESSION
-}
